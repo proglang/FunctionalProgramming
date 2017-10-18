db45f64c1c5f3ff63903d9eae8c865ac47eedcc1
Peter Thiemann <peter.thiemann@gmail.com>
2017-10-18 11:56:51 +0200

    pre-lecture-03

--- code/Examples.hs
+++ code/Examples.hs
@@ -1,1 +1,24 @@
+import Test.QuickCheck
+
 dollarRate = 1.3671
+
+-- |convert euros to usd
+usd euros = euros * dollarRate
+
+-- |convert usd to eur
+euro usds = usds / dollarRate
+
+-- |euro and usd are inverses
+prop_EuroUSD x = euro (usd x) == x
+
+-- |almost equals
+x ~== y = abs (x - y) <= abs x * 10E-15
+
+-- |euro and usd are almost inverses
+prop_EuroUSD' x = euro (usd x) ~== x
+
+price = 79
+
+price' :: Double
+price' = 79
+
