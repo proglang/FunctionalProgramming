1707303e59ecc45d4c7580dd6934a6170431f66c
Peter Thiemann <peter.thiemann@gmail.com>
2019-07-10 15:28:34 +0200

    more examples church encoding

--- code2019/M17lambda.hs
+++ code2019/M17lambda.hs
@@ -1,5 +1,5 @@
 
-
+{-
 -- free and bound
 ex0 = \ x -> \ y -> x
 -- free(ex0) = {}
@@ -39,3 +39,74 @@ se1 = x  |-> ne
 (\ x -> (y x)) M -beta-> (y M)
 
 (\ x -> (y x)) -eta-> y
+
+-}
+
+{-
+a lambda term M has at most one normal form
+
+Suppose
+M -b*-> N1 a normal form
+M -b*-> N2 another normal form
+
+hence N1 <-b*-> N2
+by CR there is some N such that\
+N1 -b*-> N
+N2 -b*-> N
+
+but N1, N2 are normal forms,
+so N1 =a= N and N =a= N2
+so N1 =a= N2
+
+-}
+
+tt = \ x y -> x
+ff = \ x y -> y
+iff = \ b t f -> b t f
+
+{-
+
+iff tt 42 57
+=
+(((\ b t f -> b t f) tt) 42) 57
+-b->
+((\ t f -> tt t f) 42) 57
+-b->
+(\ f -> tt 42 f) 57
+-b->
+tt 42 57
+=
+((\ x y -> x) 42) 57
+-b->
+(\ y -> 42) 57
+-b->
+42
+
+-}
+
+zero = \f x -> x
+one  = \f x -> f x
+two  = \f x -> f (f x)
+three= \f x -> f (f (f x))
+
+succ1 = \n f x -> f (n f x)
+succ2 = \n f x -> n f (f x)
+
+{-
+
+m f x = f^m x
+n f x = f^n x
+
+(\ g -> m n g) g y =
+(\ g -> n^m g) g y =
+n^m g y =
+g^(n^m) y
+
+two f x = f (f x)
+three f x = f (f (f x))
+
+two three g x =
+three (three g) x =
+(three g (three g (three g x)))
+
+-}
