c2a33c6799b51f153852a30afd4e7c4f7f5bd53d
Peter Thiemann <peter.thiemann@gmail.com>
2024-11-26 15:55:46 +0100

    during lecture

--- code2024/src/V20241126.hs
+++ code2024/src/V20241126.hs
@@ -1,3 +1,22 @@
 module V20241126 where
 
 
+data Term =
+  Con Integer
+  | Bin Term Op Term
+
+data Op = Add | Sub | Mul | Div
+
+eval :: Monad m => Term -> m Integer
+eval (Con n) = return n
+eval (Bin l o r) = do
+  v <- eval l
+  w <- eval r
+  return (sys o v w)
+
+sys :: Op -> Integer -> Integer -> Integer
+sys Add = (+)
+sys Sub = (-)
+sys Mul = (*)
+sys Div = div
+
